d40b77ab58319d524691d7db998cc6f32086f067
Nicolás Marino <nicolasmarino.17@gmail.com>
2019-02-05 23:08:41 -0300

    Basic db setup (design, connect)
    Using mysql from XAMPP

---
+++ links-app/src/keys.js
@@ -0,0 +1,8 @@
+module.exports ={
+    database:{
+        host: 'localhost',
+        user: 'nicoroot',
+        password: 'Password01',
+        database: 'links_app'
+    }
+};
--- links-app/src/routes/links.js
+++ links-app/src/routes/links.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const pool = require('../database'); // connection to db
+
+
+
 
 
 module.exports = router;
