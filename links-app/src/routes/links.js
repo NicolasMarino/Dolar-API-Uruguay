37e3ed30b9abdff4f94e0e01b3c8713b579c9555
Nicolás Marino <nicolasmarino.17@gmail.com>
2019-02-06 00:31:36 -0300

    basic routing, design form

--- links-app/src/routes/links.js
+++ links-app/src/routes/links.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const pool = require('../database'); // connection to db
 
+router.get('/add', (req, res) => {
+    res.render('links/add')
+});
 
-
+router.post('/add', (req, res) => {
+    res.send('recived');
+});
 
 
 module.exports = router;
