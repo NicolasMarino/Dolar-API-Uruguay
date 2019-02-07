26a33f99281f68eb030358973231ca869940c750
Nicolás Marino <nicolasmarino.17@gmail.com>
2019-02-06 22:54:00 -0300

    CRUD messages status ok

--- links-app/src/index.js
+++ links-app/src/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const flash = require('connect-flash');
+const sessions = require('express-session');
+const MYSQLStore = require('express-mysql-session');
+const {database} = require('./keys');
+
+
 // Initializations
 const app = express();
 
@@ -19,15 +25,23 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs');
 
 // Middlewares
+app.use(sessions({
+    secret: 'niconodesession',
+    resave: false,
+    saveUninitialized: false,
+    store: new MYSQLStore(database)
+}))
+app.use(flash());
 app.use(morgan('dev'));// ver peticiones q llegan al sv
 app.use(express.urlencoded({extended: false})); //Aceptar desde el form los datos que envian los user. datos comunes
 app.use(express.json());
+
 // Global Variables
 
-// app.use((req,res,next) => {
-
-//     next();
-// });
+app.use((req,res,next) => {
+    app.locals.success = req.flash('success');
+    next();
+});
 
 // Routes
 app.use(require('./routes'));
