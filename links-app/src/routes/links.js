39ecd9df5d2f6ea60ce46d5347773dfecbd7bcc1
Nicolás Marino <nicolasmarino.17@gmail.com>
2019-02-06 21:43:50 -0300

    CRUD: U

--- links-app/src/routes/links.js
+++ links-app/src/routes/links.js
@@ -27,7 +27,26 @@ router.get('/delete/:id', async (req,res) => {
     const { id } = req.params;
     await pool.query('DELETE FROM links WHERE ID = ?',[id]);
     res.redirect('/links');
-})
+});
+
+router.get('/edit/:id', async (req,res) => { 
+    const { id } = req.params;
+    const links = await pool.query('SELECT * FROM links WHERE ID = ?',[id]);
+    res.render('links/edit', {link: links[0]});
+});
+
+router.post('/edit/:id', async (req,res) => { 
+    const { id } = req.params;
+    const { title, description, url } = req.body;
+    const newLink = {
+        title,
+        description,
+        url
+    };
+    console.log(newLink);
+    await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
+    res.redirect('/links');
+});
 
 
 module.exports = router;
