32d9a798130fc7c8daa3920ab6b5c51173620e2c
Nicolás Marino <nicolasmarino.17@gmail.com>
2019-02-06 21:12:13 -0300

    CRUD: D

--- links-app/src/routes/links.js
+++ links-app/src/routes/links.js
@@ -23,5 +23,11 @@ router.get('/', async (req, res) => {
     res.render('links/list', {links});
 });
 
+router.get('/delete/:id', async (req,res) => { 
+    const { id } = req.params;
+    await pool.query('DELETE FROM links WHERE ID = ?',[id]);
+    res.redirect('/links');
+})
+
 
 module.exports = router;
