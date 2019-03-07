26edb20581df80c95d3bf8b37942e9be258f977c
NicolasMarino <nicolasmarino.17@gmail.com>
2019-03-07 20:19:40 -0300

    aa

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -5,3 +5,4 @@ setTimeout(function() {
 setTimeout(function() {
     $('#justMessage').fadeOut('slow');
 }, 1000);
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    const http = require('http');
+    const fs = require('fs');
+    const file = fs.createWriteStream("archivo.xls");
+    const request = http.get("http://www.ine.gub.uy/c/document_library/get_file?uuid=1dcbe20a-153b-4caf-84a7-7a030d109471", function(response) {
+      response.pipe(file);   
+    });
+    
     res.render('index');
 })
 
