9408035e84df17a29d52ca15f3eb811b58da8023
NicolasMarino <nicolasmarino.17@gmail.com>
2019-03-07 22:52:34 -0300

    Getting exchange rate in json

--- links-app/src/routes/index.js
+++ links-app/src/routes/index.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const xlstojson = require("xls-to-json");
 
 router.get('/', (req, res) => {
+    const http = require('http');
+    const fs = require('fs');
+
+    const file = fs.createWriteStream("cotizaciones.xlsx");
+    const request = http.get("http://www.ine.gub.uy/c/document_library/get_file?uuid=1dcbe20a-153b-4caf-84a7-7a030d109471", function(response) {
+      response.pipe(file);
+    });
     res.render('index');
-})
+});
+
+router.get('/apiarchivo', (req,res) => {
+  xlstojson({
+    input: "./cotizaciones.xlsx",  // input xls
+    output: "cotizaciones.json", // output json
+    lowerCaseHeaders:true
+  }, function(err, result) {
+    if(err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
 
 module.exports = router;
--- links-app/src/routes/profile.js
+++ links-app/src/routes/profile.js
@@ -6,9 +6,10 @@ const {isLoggedIn} = require('../lib/auth');
 const pool = require('../database'); // connection to db
 
 
-router.get('/', isLoggedIn, async (req, res) => {   
+router.get('/', isLoggedIn, async (req, res) => {
     const links = await pool.query('SELECT links.id, title, url, created_at, username, is_private FROM links, users WHERE links.user_id = ? AND users.id = ?', [req.user.id,req.user.id]);
     res.render('profile/main-profile', {links});
+
 });
 
 
