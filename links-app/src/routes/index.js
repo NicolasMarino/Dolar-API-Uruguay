b379bcf1c38a51c3107368d63a39a1e2d195dc70
NicolasMarino <nicolasmarino.17@gmail.com>
2019-03-08 20:26:18 -0300

    /apiarchivo, TODO: FIX xls to json

--- links-app/src/routes/index.js
+++ links-app/src/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     const http = require('http');
     const fs = require('fs');
 
-    const file = fs.createWriteStream("cotizaciones.xlsx");
+    const file = fs.createWriteStream("cotizaciones.xls");
     const request = http.get("http://www.ine.gub.uy/c/document_library/get_file?uuid=1dcbe20a-153b-4caf-84a7-7a030d109471", function(response) {
       response.pipe(file);
     });
@@ -15,8 +15,8 @@ router.get('/', (req, res) => {
 
 router.get('/apiarchivo', (req,res) => {
   xlstojson({
-    input: "./cotizaciones.xlsx",  // input xls
-    output: "cotizaciones.json", // output json
+    input: "./cotizaciones.xls", 
+    output: "cotizaciones.json",
     lowerCaseHeaders:true
   }, function(err, result) {
     if(err) {
