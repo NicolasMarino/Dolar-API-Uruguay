922060fb3f098583e402c5b2cb46bcb5b15117a1
NicolasMarino <nicolasmarino.17@gmail.com>
2019-03-19 00:34:42 -0300

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fs = require("fs");
+const XLSX = require('xlsx');
 
 router.get('/', (req, res) => {
     const http = require('http');
@@ -14,15 +15,18 @@ router.get('/', (req, res) => {
 });
 
 router.get('/api/archivo', (req,res) => {
-  
-  const XLSX = require('xlsx');
+    
   var workbook = XLSX.readFile('./cotizaciones.xls');
   XLSX.writeFile(workbook, 'cotizaciones2.xlsx');
   var workbook2 = XLSX.readFile('cotizaciones2.xlsx',{sheetStubs: true});
   var sheet_name_list = workbook2.SheetNames;
 
   var datosNuevos = [];
+  var datosNuevos2 = [];
   var auxRow = 1;
+  var jsonData = [];
+
+  var exchangeRatesJson;
 
   sheet_name_list.forEach(function(y) {
   var worksheet = workbook.Sheets[y];
@@ -49,106 +53,77 @@ router.get('/api/archivo', (req,res) => {
   }
   datosNuevos.shift();
   datosNuevos.shift();
-  //console.log(datosNuevos);
- 
-  let pathra = "dataTest.json";
-
-  fs.writeFile(pathra,JSON.stringify(datosNuevos,null,2), (err) => {
-    if (err) throw err;
-  });
-  var datos;
-  fs.readFile('./dataTest.json','utf8', function read(err, data) {
-    if (err) throw err;
-    datos =JSON.parse(data);
-    for(var i = 1; i < datos.length;i++){
-
-      if(datos[i] !== null){
-        var linea = datos[i].replace('undefined','');
-        linea = linea.split(',');
-        var contadorRepetidos=0;
-        for(var x = 0; x < linea.length;x++){
-          if(linea[0] == linea[x+1]){
-            contadorRepetidos++;
-          }
-        }
-        linea.splice(0,contadorRepetidos,'');
-        //if(linea[2])
-        //console.log(contadorRepetidos);
-        //var linea = datos[i].split(",");
-        //if()
-        //console.log(linea);
-      }
-    }  
-  });
-
-  });
-  //console.log()
-  var output = JSON.stringify(datosNuevos,null,2);
-
-  res.render('index', {archivo: output});
-});
-router.get('/api/listado', (req,res) => {
-  const XLSX = require('xlsx');
-  var workbook2 = XLSX.readFile('cotizaciones2.xlsx');
-  var sheet_name_list = workbook2.SheetNames;
-
-  sheet_name_list.forEach(function (y) { /* iterate through sheets */
-    //var exceljsonObj = [];
-    var rowObject  =  XLSX.utils.sheet_to_row_object_array(workbook2.Sheets[y]);
-    exceljsonObj = rowObject;
-        for(var i=0;i<exceljsonObj.length;i++){
-        //var recordcount = exceljsonObj.length;
-        var data = exceljsonObj[i];
-        // $('#myTable tbody:last-child').append("<tr><td>"+data.ID+"</td><td>"+data.Name+"</td><td>"+data.Months+"</td></tr>");
-        // }
-        //console.log(data);
-    //alert(exceljsonObj.length);
-       
-    };
-  });
-
-  //var first_sheet_name = workbook2.SheetNames[0];
   
-  var sheet = workbook2.Sheets[workbook2.SheetNames[0]];
-  var range = XLSX.utils.decode_range(sheet['!ref']);
-  console.log(range.s.r);
-  console.log(range.e.r);
+  for(var i = 1; i < datosNuevos.length-1;i++){
 
+    if(datosNuevos[i] !== undefined){
+      var linea = datosNuevos[i].replace('undefined','');
+      linea = linea.split(',');
+      var contadorRepetidos=0;
+      
 
-  // /* Get worksheet */
-  // var worksheet = workbook2.Sheets[first_sheet_name];
-
-  // /* Find desired cell */
-  // var desired_cell = worksheet[address_of_cell];
-
-  // /* Get the value */
-  // var desired_value = (desired_cell ? desired_cell.v : undefined);
-  // console.log(desired_value);
-  //   var datos;
-  //  /// var stream = fs.createReadStream('./output.json');
-  //   //stream.setEncoding('utf8');
-  //   var datos = JSON.parse("./output.json")
-    
-  //   console.log(datos);
-    
-
-  //   res.send(datos);
-});
-router.get('/api/archivo/test', (req,res) => {
-  function to_json(workbook) {
-    var result = {};
-    workbook.SheetNames.forEach(function(sheetName) {
-      var roa = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-      if(roa.length > 0){
-        result[sheetName] = roa;
+      for(var x = 0; x < linea.length;x++){
+        if(linea[0] == linea[x+1]){
+          contadorRepetidos++;
+        }          
       }
-    });
-    return result;
+      linea.splice(0,contadorRepetidos,'');
+      var meses = ["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"];
+      var yearsFixThis = ["1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2011","2012","2013","2014","2015","2016","2017","2018","2019"];
+      var exchangeRates = new Object();
+      var lastMonth;
+      
+      var lastYear;
+      if(linea[4] != undefined){
+        if(yearsFixThis.includes(linea[4])){
+          exchangeRates.years = linea[4];
+          lastYear = linea[4];
+        }else{
+          exchangeRates.years = lastYear;
+        }
+      } 
+      if(linea[3] != undefined){  
+            
+        if(meses.includes(linea[3])){          
+          linea.splice(3,0);
+          exchangeRates.month = linea[3];
+          exchangeRates.day= linea[1];  
+          exchangeRates.buyDollar= linea[4];
+          exchangeRates.sellDollar= linea[5];
+          lastMonth= linea[3];
+        }else{
+          exchangeRates.month = lastMonth;
+          exchangeRates.day= linea[1];  
+          exchangeRates.buyDollar= linea[3];
+          exchangeRates.sellDollar= linea[4];
+        }
+      }      
+      exchangeRatesJson = JSON.stringify(exchangeRates,null,2);
+      jsonData.push(exchangeRatesJson);
+      
+      datosNuevos2.push(JSON.parse(JSON.stringify(linea,null,2)));
+    };
   };
-  const XLSX = require('xlsx');
-  var workbook2 = XLSX.readFile('cotizaciones2.xlsx');
-  var jsonDataa = to_json(workbook2);
-  console.log(jsonDataa);
+  
+  // for(var opa =0;opa<jsonData.length;opa++){
+  //   console.log(JSON.parse(jsonData[opa]));
+  // }
+
+  // let pathra = "dataTest.json";
+
+  // fs.writeFile(pathra,JSON.stringify(datosNuevos2,null,2), (err) => {
+  //   if (err) throw err;
+  // });
+  // let path = "dataTest2.json";
+
+  // fs.writeFile(path,jsonData, (err) => {
+  //   if (err) throw err;
+  // });
+
+  });
+
+  res.render('index', {archivo: jsonData});
 });
+
 
 module.exports = router;
