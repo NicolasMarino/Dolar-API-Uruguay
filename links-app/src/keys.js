08a27f463d4e43deca4150859ea8ff43d3a16dbd
NicolasMarino <nicolasmarino.17@gmail.com>
2019-03-22 01:01:44 -0300

    TODO: fix heroku
    rompi todo heroku. 22/3/19 1:01

--- links-app/src/keys.js
+++ links-app/src/keys.js
@@ -1,7 +1,7 @@
 module.exports ={
     database:{
-        host: 'localhost',
-        user: 'nicoroot',
+        host: 'linksapp.c2bk16ev2b66.sa-east-1.rds.amazonaws.com',
+        user: 'nico',
         password: 'Password01',
         database: 'linksapp'
     }
