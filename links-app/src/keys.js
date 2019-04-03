9c6ab7afd13b8a213835996d0b08072b7053b064
Nicolas Marino <nicolasmarino.17@gmail.com>
2019-04-03 14:16:22 -0300

    Revert "name change"
    This reverts commit 6b9cadb39bbd93ab5f04079e4cb32bfafa29fbf7.

---
+++ links-app/src/keys.js
@@ -0,0 +1,8 @@
+module.exports ={
+    database:{
+        host: 'linksapp.c2bk16ev2b66.sa-east-1.rds.amazonaws.com',
+        user: 'nico',
+        password: 'Password01',
+        database: 'linksapp'
+    }
+};
---
+++ links-app/src/routes/links.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const {isLoggedIn} = require('../lib/auth');
+
+const pool = require('../database'); // connection to db
+
+router.get('/add', isLoggedIn, (req, res) => {
+    res.render('links/add')
+});
+
+router.post('/add', isLoggedIn, async (req, res) => {
+    const { title, url, description } = req.body;
+    var {is_private} = req.body;
+    if(is_private == "on"){
+        is_private = true;
+    }else{
+        is_private = false;
+    }
+    const newLink = {
+        title,
+        url,
+        description,
+        user_id: req.user.id,
+        is_private
+    };
+    console.log(newLink);
+    await pool.query('INSERT INTO links set ?',[newLink]);
+    req.flash('success', 'Link saved succesfully');
+    res.redirect('/profile');
+});
+
+router.get('/', isLoggedIn, async (req, res) => {
+    const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
+    res.render('links/list', {links});
+});
+
+router.get('/delete/:id', isLoggedIn, async (req,res) => { 
+    const { id } = req.params;
+    await pool.query('DELETE FROM links WHERE ID = ?',[id]);
+    req.flash('success', 'Link removed succesfully');
+    res.redirect('/profile');
+});
+
+router.get('/edit/:id', isLoggedIn, async (req,res) => { 
+    const { id } = req.params;
+    const links = await pool.query('SELECT * FROM links WHERE ID = ?',[id]);
+    res.render('links/edit', {link: links[0]});
+});
+
+router.post('/edit/:id', isLoggedIn, async (req,res) => { 
+    const { id } = req.params;    
+    const { title, description, url } = req.body;
+    var {is_private} = req.body;
+    if(is_private == "on"){
+        is_private = true;
+    }else{
+        is_private = false;
+    }
+    const newLink = {
+        title,
+        description,
+        url,
+        is_private
+    };
+    console.log(newLink);
+    await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
+    req.flash('success', 'Link updated succesfully');
+    res.redirect('/profile');
+});
+
+router.get('/all', isLoggedIn, async (req,res) => { 
+    const links = await pool.query('SELECT title, url, created_at, username, is_private FROM links, users where links.user_id = users.id and links.is_private = 0');
+    res.render('links/all-links', {links});
+});
+
+
+module.exports = router;
