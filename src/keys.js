5c0e1045f506d67e51f2eb01b61d00eb476dcbb5
Nicolas Marino <nicolasmarino.17@gmail.com>
2019-04-04 17:55:13 -0300

    fix

--- src/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports ={
-    database:{
-        host: 'linksapp.c2bk16ev2b66.sa-east-1.rds.amazonaws.com',
-        user: 'nico',
-        password: 'Password01',
-        database: 'linksapp'
-    }
-};
--- src/public/js/main.js
+++
@@ -1,7 +0,0 @@
-
-setTimeout(function() {
-    $('#messageSuccess').fadeOut('slow');
-}, 1000);
-setTimeout(function() {
-    $('#justMessage').fadeOut('slow');
-}, 1000);
